c2d927c1de10f91387c58e4519d12b076ddcc1bd
Ayush <usersynax@gmail.com>
2026-04-21 19:23:04 +0530

    Update User model with authentication fields

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String },
   avatar: { type: String },
   bio: { type: String },
   country: { type: String },
