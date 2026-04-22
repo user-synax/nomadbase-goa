f28a88e39316f21def5d80cd539eb557d747729c
Ayush <usersynax@gmail.com>
2026-04-22 08:49:21 +0530

    Remove profile route directory

--- app/(root)/profile/[username]/page.js
+++
@@ -1,6 +0,0 @@
-import UserProfile from "@/components/profile/UserProfile";
-
-export default async function ProfilePage({ params }) {
-  const { username } = await params;
-  return <UserProfile username={username} />;
-}
